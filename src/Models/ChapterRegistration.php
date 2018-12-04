8d40a24e5667db68be29529de0772c3f6ecd618f
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-12-04 02:24:25 +0100

    Updated ChapterRegistration model

--- src/Models/ChapterRegistration.php
+++ src/Models/ChapterRegistration.php
@@ -5,17 +5,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChapterRegistration extends Model {
   protected $table = 'chapter_registrations';
+  public $primaryKey = 'chapter_name';
   public $timestamp = false;
   public $incrementing = true;
   protected $fillable = [
-    'chapter_name', 'transaction_ref', 'created_at'
+    'chapter_name',
+    'transaction_ref'
   ];
 
   public function getPayload() {
     return [
       'chapterName' => $this->chapter_name,
       'transactionRef' => $this->transaction_ref,
-      'createdAt' => $this->created_at
+      'createdAt' => $this->created_at,
+      'updatedAt' => $this->updated_at
     ];
   }
 }
