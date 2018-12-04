e7f55bdb18b12bd22526ef46bbbad6dc68d46ad7
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-12-04 02:22:31 +0100

    Updated ChapterDue model

--- src/Models/ChapterDue.php
+++ src/Models/ChapterDue.php
@@ -5,37 +5,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChapterDue extends Model {
   protected $table = 'chapter_dues';
-  public $timestamp = false;
-  public $incrementing = true;
+  public $primaryKey = 'chapter_name';
+  public $timestamp = true;
+  public $incrementing = false;
   protected $fillable = [
-    'mrn',
-    'surname',
-    'firstname',
-    'othername',
-    'gender_id',
-    'phone',
-    'date_of_birth',
-    'photo',
-    'twitter',
-    'facebook',
-    'linkedin',
-    'website'
+    'chapter_name',
+    'transaction_ref'
   ];
 
   public function getPayload() {
     return [
-      'mrn' => $this->mrn,
-      'surname' => $this->surname,
-      'firstName' => $this->firstname,
-      'otherName' => $this->othername,
-      'genderId' => $this->gender_id,
-      'phone' => $this->phone,
-      'dateOfBirth' => $this->date_of_birth,
-      'photo' => $this->photo,
-      'twitter' => $this->twitter,
-      'facebook' => $this->facebook,
-      'linkedin' => $this->linkedin,
-      'website' => $this->website
+      'chapter_name' => $this->chapter_name,
+      'transactionRef' => $this->transaction_ref,
+      'createdAt' => $this->created_at,
+      'updatedAt' => $this->updated_at
     ];
   }
 }
