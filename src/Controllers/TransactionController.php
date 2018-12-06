7ee09bd6f992865d4aebe43c3055bc6b04925f33
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-12-06 17:21:41 +0100

    Updated transaction controller

--- src/Controllers/TransactionController.php
+++ src/Controllers/TransactionController.php
@@ -16,9 +16,7 @@ use Slim\Http\Response;
 class TransactionController extends Controller {
   protected $requiredParams = [
     'email',
-    'phone',
     'transaction_ref',
-    'amount',
     'response_code',
     'response_message'
   ];
@@ -27,7 +25,7 @@ class TransactionController extends Controller {
     parent::__construct($container);
   }
 
-  public function addTransaction(Request $request, Response $response) {
+  public function logTransaction(Request $request, Response $response) {
     $endpoint = $this->getPath($request);
     $params = $request->getParsedBody();
 
@@ -37,18 +35,16 @@ class TransactionController extends Controller {
     }
 
     try {
-      $transaction = new Transaction();
+      $transaction = Transaction::firstOrNew(['transaction_ref'=> $params['transaction_ref']]);
       $transaction->email = strtolower($params['email']);
-      $transaction->phone = $params['phone'];
-      $transaction->transaction_ref = $params['transaction_ref'];
-      $transaction->amount = $params['amount'];
+      $transaction->phone = ($params['phone']) ? $params['phone'] : null;
       $transaction->response_code = $params['response_code'];
       $transaction->response_message = $params['response_message'];
       $transaction->save();
 
       $transactionPayload = $transaction->fresh()->getPayload();
 
-      return $response->withJson(['status'=> true, 'message'=> 'Transaction logged successfully!', "treaction"=> $transactionPayload], 200);
+      return $response->withJson(['status'=> true, 'message'=> 'Transaction logged successfully!', "transaction"=> $transactionPayload], 200);
     } catch (QueryException $dbException) {
       $databaseErrorPayload = $this->getDatabaseErrorPayload($endpoint, $dbException);
       return $response->withJson($databaseErrorPayload, 500);
