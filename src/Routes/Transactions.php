7c25a2400aa011e2c9b132daf5c9dea0c300df77
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-12-06 17:22:15 +0100

    Updated transaction routes

--- src/Routes/Transactions.php
+++ src/Routes/Transactions.php
@@ -2,5 +2,5 @@
 
 use NACOSS\Controllers\TransactionController;
 
-$this->post('', TransactionController::class . ':addTransaction');
+$this->post('', TransactionController::class . ':logTransaction');
 $this->get('', TransactionController::class . ':getAllTransactions');
