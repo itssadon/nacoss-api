83e3246a52a3c373a850a10013d0147806e5664a
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-12-09 08:30:40 +0100

    Added chapter search route

--- src/Routes/Chapters.php
+++ src/Routes/Chapters.php
@@ -4,6 +4,7 @@ use NACOSS\Controllers\ChapterController;
 
 $this->post('', ChapterController::class . ':addChapter');
 $this->put('', ChapterController::class . ':updateChapterRegistration');
+$this->get('/search/{search-term}', ChapterController::class . ':searchChapter');
 $this->get('', ChapterController::class . ':getAllChapters');
 $this->get('/active', ChapterController::class . ':getActiveChapters');
 $this->post('/activate', ChapterController::class . ':updateChapterDue');
