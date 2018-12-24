d161af9bf588331215fd1457f98076ba392c7d0a
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-12-24 23:37:35 +0100

    Add endpoint for updating member details

--- src/Routes/Members.php
+++ src/Routes/Members.php
@@ -5,3 +5,4 @@ use NACOSS\Controllers\MemberController;
 $this->post('', MemberController::class . ':addMember');
 $this->get('', MemberController::class . ':getAllMembers');
 $this->get('/{mrn}', MemberController::class . ':getMemberDetails');
+$this->put('/{mrn}', MemberController::class . ':updateMemberDetails');
