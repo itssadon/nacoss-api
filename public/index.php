b6f168cf6c694815990a6cd5ec5fd9d2ad28c4b5
Sadiq Hassan <abubakarhassan59@gmail.com>
2019-01-02 09:10:21 +0100

    Removed code not working

--- public/index.php
+++ public/index.php
@@ -35,19 +35,6 @@ require 'src/dependencies.php';
 // Register middleware
 require 'src/middleware.php';
 
-// CORS
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
-});
-
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
-
 // Register routes
 require 'src/routes.php';
 
