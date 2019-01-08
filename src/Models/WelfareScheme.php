459c5ceeb91ffc386091724694a8a69a9a64a844
Sadiq Hassan <abubakarhassan59@gmail.com>
2019-01-08 18:26:40 +0100

    Added Welfare scheme model

---
+++ src/Models/WelfareScheme.php
@@ -0,0 +1,31 @@
+<?php
+namespace NACOSS\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WelfareScheme extends Model {
+  protected $table = 'welfare_scheme';
+  public $primaryKey = 'mrn';
+  public $timestamp = false;
+  public $incrementing = false;
+  protected $fillable = [
+    'mrn',
+    'cover_year',
+    'beneficiary_name',
+    'beneficiary_phone'
+  ];
+  
+  protected $hidden = [
+    'password'
+  ];
+
+  public function getPayload() {
+    return [
+      'mrn' => $this->mrn,
+      'coverYear' => $this->cover_year,
+      'beneficiary_name' => $this->beneficiary_name,
+      'beneficiary_phone' => $this->beneficiary_phone
+    ];
+  }
+
+}
