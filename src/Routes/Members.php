5a72f863929420a556b27088f47ccb998b0beaed
Sadiq Hassan <abubakarhassan59@gmail.com>
2019-06-18 00:29:10 +0100

    Add member search route

--- src/Routes/Members.php
+++ src/Routes/Members.php
@@ -6,5 +6,6 @@ $this->post('', MemberController::class . ':addMember');
 $this->get('', MemberController::class . ':getAllMembers');
 $this->get('/uncovered', MemberController::class . ':getUncoveredMembers');
 $this->post('/cover', MemberController::class . ':insureMember');
+$this->get('/search', MemberController::class . ':searchMember');
 $this->get('/{mrn}', MemberController::class . ':getMemberDetails');
 $this->put('/{mrn}', MemberController::class . ':updateMemberDetails');
